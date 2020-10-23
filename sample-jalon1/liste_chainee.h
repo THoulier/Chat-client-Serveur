2a4e97b74460ec1f00b94e6cdf7cb91d2bca4e03
Thomas Houlier <thoulier@enseirb-matmeca.fr>
2020-10-23 22:42:48 +0200

    petit fix

--- sample-jalon1/liste_chainee.h
+++ sample-jalon1/liste_chainee.h
@@ -17,25 +17,3 @@ void suppression(struct list_client * list);
 void display_list(struct list_client * list);
 
 
-
-
-
-
-
-
-
-/*
-struct list_client_datas{
-	struct client_datas * client;
-	struct list_client_datas * next;
-};
-
-struct client_datas{
-	int fd;
-	int port;
-	char * adress;
-};
-
-void client_sup(struct client_datas * client);
-void client_insertion(int fd, int port, char * adress);
-*/
